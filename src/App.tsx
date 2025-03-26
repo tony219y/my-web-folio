bf69964ee4bca4cf28ab3fd6a59b68c095ec8d40
tony007x <aekanan2004@gmail.com>
2025-03-26 10:39:26 +0700

    EmailForm

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Projects from "./components/Projects"
 import Profile from "./components/Profile"
 import Experience from "./components/Experience"
 import Techstack from "./components/Techstack"
+import ContactMail from "./components/ContactMail"
 function App() {
   return (
     <>
@@ -23,8 +24,12 @@ function App() {
       <div id="tools" className="flex w-full justify-center md:justify-end px-4 md:px-6 lg:px-8 z-10">
         <Techstack />
       </div>
-      <div id="send-message" className="flex w-full min-h-screen h-auto justify-center md:justify-end px-4 md:px-6 lg:px-8 z-10">
+      <div id="send-message" className="flex w-full min-h-screen h-screen justify-center md:justify-end px-4 md:px-6 lg:px-8 z-10">
+        <ContactMail />
       </div>
+      <footer className="flex w-full justify-center md:justify-end px-4 md:px-6 lg:px-8 z-10">
+        <p className="text-white/50 text-sm">© 2025 Akeanant Poomdeesittinon. All rights reserved.</p>
+      </footer>
     </>
   )
 }
---
+++ src/components/ContactMail.tsx
@@ -0,0 +1,80 @@
+import { useState } from "react"
+
+function ContactMail() {
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [subject, setSubject] = useState("")
+    const [message, setMessage] = useState("")
+
+    return (
+        <div className="flex flex-col w-[60%] max-lg:w-full z-10">
+            <div className="flex flex-col w-full h-full justify-center items-start pt-16 gap-4 p-4 max-lg:items-center rounded-xl max-md:gap-4">
+                <h1 className="text-white text-8xl font-bold 2xl:text-8xl max-md:text-4xl">SEND ME</h1>
+                <h1 className="text-white/20 text-8xl font-bold 2xl:text-8xl max-md:text-4xl">A MESSAGE</h1>
+                <div className="flex flex-col w-full h-full justify-center items-start pt-8 gap-4 p-4 max-lg:items-center rounded-xl">
+                    <form 
+                        action="https://formsubmit.co/f2ac7c1857f7dd5905fb3b7eb6c72938" 
+                        method="POST" 
+                        className="flex flex-col w-full h-full gap-4"
+                    >
+                        <div className="flex flex-col w-full h-full gap-4">
+                            <div className="flex w-full gap-4 max-md:flex-col">
+                                <div className="flex flex-col w-full gap-2">
+                                    <label htmlFor="name" className="text-white">Name</label>
+                                    <input 
+                                        type="text" 
+                                        name="name" 
+                                        placeholder="Name" 
+                                        className="w-full p-2 pl-4 rounded-xl bg-white/30 text-white placeholder:text-white outline-none" 
+                                        required 
+                                        onChange={(e) => setName(e.target.value)} 
+                                    />
+                                </div>
+                                <div className="flex flex-col w-full gap-2">
+                                    <label htmlFor="email" className="text-white">Email</label>
+                                    <input 
+                                        type="email" 
+                                        name="email" 
+                                        placeholder="Email" 
+                                        className="w-full p-2 pl-4 rounded-xl bg-white/30 text-white placeholder:text-white outline-none" 
+                                        required 
+                                        onChange={(e) => setEmail(e.target.value)} 
+                                    />
+                                </div>
+                            </div>
+                            <div className="flex flex-col w-full gap-2">
+                                <label htmlFor="subject" className="text-white">Subject</label>
+                                <input 
+                                    type="text" 
+                                    name="subject" 
+                                    placeholder="Subject" 
+                                    className="w-full p-2 pl-4 rounded-xl bg-white/30 text-white placeholder:text-white outline-none" 
+                                    required 
+                                    onChange={(e) => setSubject(e.target.value)} 
+                                />
+                            </div>
+                            <div className="flex flex-col w-full gap-2">
+                                <label htmlFor="message" className="text-white">Message</label>
+                                <textarea 
+                                    name="message" 
+                                    placeholder="Message" 
+                                    className="w-full min-h-[200px] p-2 pl-4 rounded-xl bg-white/30 text-white placeholder:text-white outline-none" 
+                                    required 
+                                    onChange={(e) => setMessage(e.target.value)} 
+                                ></textarea>
+                            </div>
+                            <button 
+                                type="submit" 
+                                className="w-full p-2 bg-white rounded-xl hover:bg-white/70 transition-all duration-300 text-black"
+                            >
+                                Send
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ContactMail
