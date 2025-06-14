335400c8917c95953c75cbc46dc9adc16258a38e
tony007x <aekanan2004@gmail.com>
2025-06-14 11:56:56 +0700

    test CI/CD

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -22,7 +22,7 @@ function Profile() {
                             className="w-full h-full object-cover rounded-xl max-xl:w-1/3 max-xl:h-1/3 max-md:w-full max-md:h-1/2" />
                     </div>
                 </div>
-                <h1 className="text-black text-2xl font-bold text-center max-md:text-xl">Akeanant Poomdeesittinon ZaZa</h1>
+                <h1 className="text-black text-2xl font-bold text-center max-md:text-xl">Akeanant ZAZA</h1>
             </div>
             {/* Information Container */}
             <div className="flex flex-col w-1/2 h-1/2 bg-black/10 backdrop-blur-sm rounded-xl gap-4 p-4 justify-around max-md:w-full">
